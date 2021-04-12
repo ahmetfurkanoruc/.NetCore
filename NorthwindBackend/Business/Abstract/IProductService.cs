cbd8021bf71e55d42fbf7cc3206798927ad7b686
ahmetfurkanoruc <41059018+ahmetfurkanoruc@users.noreply.github.com>
2021-04-12 23:35:20 +0300

    This is a project that use web api and layered structure.

---
+++ NorthwindBackend/Business/Abstract/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<List<Product>> GetList();
+        IDataResult<List<Product>> GetListByCategory(int categoryId);
+        IDataResult<Product> GetById(int productId);
+        IResult Add(Product product);
+        IResult Update(Product product);
+        IResult Delete(Product product);
+
+    }
+}
